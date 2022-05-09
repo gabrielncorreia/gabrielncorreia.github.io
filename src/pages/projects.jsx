2d43593570faa0b95129d349b647394f28e69108
Gabriel <gncorreia@protonmail.com>
2022-05-09 20:21:59 -0300

    página de projetos modificada

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/react-in-jsx-scope */
 import './projects.css';
-import artWithPixels from '../images/art-with-pixels.gif';
+// import artWithPixels from '../images/art-with-pixels.gif';
 import gif1 from '../images/iai0p5j9cuszzpqxdmws.gif';
 // import gif2 from '../images/1444070216325387.gif';
 
@@ -16,21 +16,82 @@ export default function Projects() {
         <h4>Page under construction!!</h4>
         <hr />
       </div>
-      <section id="projects-section">
+      <section id="all-projects-section">
         <div className="projetos-div">
-          <h3>Projetos do Módulo de Fundamento</h3>
-          <div className="projetos-fundamento-div">
-            <div className="projeto-fundamento-1">
-              <img src={artWithPixels} width="250px" alt="" />
-              <p>O objetivo desse trabalho...</p>
+          <div className="projeto-div">
+            <div className="div-img" />
+            <h4>Recipe App</h4>
+            <p>O objetivo desse trabalho</p>
+            <div className="repo-live-div">
+              <h5>Repositório</h5>
+              <h5>Live Demo</h5>
             </div>
-            <div className="projeto-fundamento-1">
-              <img src={artWithPixels} width="250px" alt="" />
-              <p>O objetivo desse trabalho...</p>
+          </div>
+          <div className="projeto-div">
+            <div className="div-img" />
+            <h4>Trivia</h4>
+            <p>O objetivo desse trabalho</p>
+            <div className="repo-live-div">
+              <h5>Repositório</h5>
+              <h5>Live Demo</h5>
+            </div>
+          </div>
+          <div className="projeto-div">
+            <div className="div-img" />
+            <h4>Trybe Futebol Clube (TFC)</h4>
+            <p>O objetivo desse trabalho</p>
+            <div className="repo-live-div">
+              <h5>Repositório</h5>
+              <h5>Live Demo</h5>
+            </div>
+          </div>
+          <div className="projeto-div">
+            <div className="div-img" />
+            <h4>Trybetunes</h4>
+            <p>O objetivo desse trabalho</p>
+            <div className="repo-live-div">
+              <h5>Repositório</h5>
+              <h5>Live Demo</h5>
+            </div>
+          </div>
+          <div className="projeto-div">
+            <div className="div-img" />
+            <h4>Front-end Online Store</h4>
+            <p>O objetivo desse trabalho</p>
+            <div className="repo-live-div">
+              <h5>Repositório</h5>
+              <h5>Live Demo</h5>
+            </div>
+          </div>
+          <div className="projeto-div">
+            <div className="div-img" />
+            <h4>TrybeWallet</h4>
+            <p>O objetivo desse trabalho</p>
+            <div className="repo-live-div">
+              <h5>Repositório</h5>
+              <h5>Live Demo</h5>
+            </div>
+          </div>
+          <div className="projeto-div">
+            <div className="div-img" />
+            <h4>Star Wars Planets</h4>
+            <p>O objetivo desse trabalho</p>
+            <div className="repo-live-div">
+              <h5>Repositório</h5>
+              <h5>Live Demo</h5>
+            </div>
+          </div>
+          <div className="projeto-div">
+            <div className="div-img" />
+            <h4>TrybeWarts</h4>
+            <p>O objetivo desse trabalho</p>
+            <div className="repo-live-div">
+              <h5>Repositório</h5>
+              <h5>Live Demo</h5>
             </div>
           </div>
         </div>
-        <div className="projetos-div">
+        {/* <div className="projetos-div">
           <h3>Projetos do Módulo de Front-End</h3>
           <div className="projetos-fundamento-div">
             <div className="projeto-fundamento-1">
@@ -42,8 +103,8 @@ export default function Projects() {
               <p>O objetivo desse trabalho...</p>
             </div>
           </div>
-        </div>
-        <div className="projetos-div">
+        </div> */}
+        {/* <div className="projetos-div">
           <h3>Projetos do Módulo de Back-End</h3>
           <div className="projetos-fundamento-div">
             <div className="projeto-fundamento-1">
@@ -55,7 +116,7 @@ export default function Projects() {
               <p>O objetivo desse trabalho...</p>
             </div>
           </div>
-        </div>
+        </div> */}
       </section>
     </main>
   );
